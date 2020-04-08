47e536737e6844ede4ded20eb9b05f62f98694a1
crmerlino <crmerlino@gmail.com>
2020-04-08 16:56:17 -0400

    AngularJs list

--- angularRest/src/main/webapp/js/app.js
+++ angularRest/src/main/webapp/js/app.js
@@ -2,8 +2,19 @@
 
 // Declare app level module which depends on filters, and services
 //
-angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
+var myApp = angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
+    config(['$routeProvider', function ($routeProvider) 
+    	{
+        	$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+        	$routeProvider.otherwise({redirectTo: '/view1'});
+    	}]);
+
+	myApp.controller('MyController', function MyController($scope,$http)
+	{
+		$http.get('js/data.json').then(function(response) 
+		{
+		   	$scope.artists = response.data;	   
+		   	$scope.artistOrder = "name";
+		});
+	});
+
