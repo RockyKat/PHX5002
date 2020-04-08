2ed6e1b35bbf15e76f2736d6925d83097916cb82
crmerlino <crmerlino@gmail.com>
2020-04-07 22:36:33 -0400

    master

--- angularRest/src/main/webapp/js/app.js
+++ angularRest/src/main/webapp/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
+//
 angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directives', 'ngdemo.controllers']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
